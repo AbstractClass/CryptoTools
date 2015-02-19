3c9e02fd76e8ae7915feb0c1dbcb9e70b25c5c78
Connor M <theabstractclass@gmail.com>
2015-02-19 13:07:32 -0500

    Fixed unfolder, only works with an EVEN amount of numbers.

--- number 'unfolder'.py
+++ number 'unfolder'.py
@@ -2,9 +2,9 @@ import math
 numbers = input("Enter numbers with spaces: ").split()
 orderedNums = []
 def oddCount(start):
-    for i in range(start,len(numbers),2):
+    for i in range(start,len(numbers), 2):
         orderedNums.append(numbers[i])
 oddCount(0)
 numbers.reverse()
-oddCount(1)
+oddCount(0)
 print(' '.join(orderedNums))
